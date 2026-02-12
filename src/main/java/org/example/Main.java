8175487b7d7b1ab2cf84282ce8366b4d28099fcd
mattheosL <mattheuslimamec@gmail.com>
2026-02-11 23:08:38 -0300

    Teste

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,6 +6,7 @@ public class Main {
     static void main() {
 
         IO.println(String.format("Hello and welcome!"));
+        IO.println(String.format("Hello"));
 
         for (int i = 1; i <= 5; i++) {
 
